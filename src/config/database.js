5dfb746b4bfe8cbbbee7518092f3213ba8e9b8f2
NeitanDev <natan.miranda02@gmail.com>
2020-04-28 13:33:44 -0300

    migrate de user criada

---
+++ src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'db4free.net',
+    username: 'kairos_consult',
+    password: 'sounousounou',
+    database: 'db_fly_van',
+    define: {
+        timestamps: true,
+        underscored: true,
+    }
+};
