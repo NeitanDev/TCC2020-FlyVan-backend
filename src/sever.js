6cb35f1bfc98ab4fcaea49b7e4a81fb1a1ebee8f
NeitanDev <natan.miranda02@gmail.com>
2020-06-14 23:16:07 -0300

    imagens visiveis pelo navegador

--- src/sever.js
+++ src/sever.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const routes = require('./routes');
 
 require('./database/index');
@@ -7,5 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
