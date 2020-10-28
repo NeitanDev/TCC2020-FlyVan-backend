d562cb3b2d8aff1927339863df6c5157846070bb
NeitanDev <natan.miranda02@gmail.com>
2020-10-28 10:47:00 -0300

    acho que agora foi

--- src/sever.js
+++ src/sever.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const routes = require('./routes');
+const cors = require('cors');
+
 
 require('./database/index');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
